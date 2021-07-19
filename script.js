c6ee773b468ebf5377f8b5a6c222d37d1622fe90
Jonny Sharrock <85681227+JS2K8@users.noreply.github.com>
2021-07-19 14:56:02 +0100

    Add files via upload
    Initial file upload

---
+++ script.js
@@ -0,0 +1,69 @@
+// Adds an item when there's input, adds nothing if input is blank or whitespace
+
+function addItem() {
+  var checkEmpty = document.getElementById("input").value;
+  if (checkEmpty.trim() !== "") {
+    var txtVal = document.getElementById("input").value,
+    node = document.createElement("li"),
+    listnode = document.getElementById("list"),
+    txtNode = document.createTextNode(txtVal);
+    node.appendChild(txtNode);
+    listnode.appendChild(node);
+    document.getElementById('input').value='';
+  }
+  else {
+    document.getElementById("input").value="";
+  }
+}
+
+// Changes the class style on click
+
+function changeClass() {
+  var newClass = document.getElementById("changeThis")
+  newClass.classList.toggle("checked");
+}
+
+// Adds crossed class to list item on click
+
+var list = document.getElementById("list");
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Double click to remove an item
+
+var list = document.getElementById("list");
+list.addEventListener('dblclick', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.remove();
+  }
+}, false);
+
+// On key press
+
+function enterPress(event) {
+  var x = event.keyCode;
+  if (x == 13) {
+    addItem()
+  }
+}
+
+// Clears the list
+
+function clearList() {
+  var list = document.getElementById("list");
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+}
+
+// Remove only list items that have the crossed class
+
+function clearCrossed() {
+  var elems = document.querySelectorAll(".checked");
+  elems.forEach(function(element) {
+    element.parentNode.removeChild(element);
+  });
+}
